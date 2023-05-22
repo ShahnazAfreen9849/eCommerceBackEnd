f6b6cdd8f9062cc96268b4991e46be9a6a1a5026
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-21 22:15:25 -0400

    edited back starter code

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    port: 3306,
-  }
-);
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
--- seeds/server.js
+++ seeds/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
